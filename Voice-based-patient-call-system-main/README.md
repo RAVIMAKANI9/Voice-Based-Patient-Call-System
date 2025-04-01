f586d9d81447be7bc5e00cabbc3548e75af0f7c1
Makani Ravi <154521505+RAVIMAKANI9@users.noreply.github.com>
2025-04-01 18:24:09 +0530

    Delete Voice-based-patient-call-system-main directory

--- Voice-based-patient-call-system-main/README.md
+++
@@ -1,1 +0,0 @@
-# Voice-based-patient-call-system
--- Voice-based-patient-call-system-main/VoiceBasedPatientCallSystem(Patient&Nurse)/patient-backend/backendNlpFiles/translation.py
+++
@@ -1,6 +0,0 @@
-from googletrans import Translator
-
-translator = Translator()
-
-def translate_text(text, target_language="en"):
-    return translator.translate(text, dest=target_language)
